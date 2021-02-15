b066c4b130a5b996778aad24c15dab3d1213f962
cromerohernandez <c.romero.hsv@gmail.com>
2021-02-15 23:52:14 +0100

    DEV: LogoSprite

--- src/components/misc/LogoSprite.js
+++ src/components/misc/LogoSprite.js
@@ -4,8 +4,35 @@ import logoSprite from '../../assets/images/taskbleLogoSprite.png'
 
 class LogoSprite extends React.Component {
   state = {
-    frame: 0,
-    logo: logoSprite
+    logo: new Image(),
+    src: logoSprite,
+    frames: 16,
+    frameIndex: 0,
+    ctx: null
+  }
+
+  setCanvas = () => {
+    const canvas = document.getElementById("my-canvas")
+    canvas.height = window.innerHeight
+    canvas.width = window.innerHeight
+    const ctx = canvas.getContext("2d")
+
+    this.setState({
+      ctx: ctx
+    })
+  }
+
+  setLogo = () => {
+    const { logo, src } = this.state
+
+    logo.src = src
+    logo.onload = () => this.draw()
+  }
+
+  drawLogo = () => {
+    this.state.ctx.drawImage(
+      this.state
+    )
   }
 
 
@@ -13,12 +40,10 @@ class LogoSprite extends React.Component {
   render() {
     const { logo } = this.state
 
+    //<img alt='logoSprite' src={logo} height='100'/>
+
     return(
-      //<canvas height='236' width='1138'>
-      <div>
-        <img alt='logoSprite' src={logo} height='100'/>
-      </div>
-      //</canvas>
+      <canvas height='236' width='1138'/>
     )
   }
 }
