ef7c7009719431dcdc516bd7336d6faca23aad54
cromerohernandez <c.romero.hsv@gmail.com>
2021-02-20 13:16:58 +0100

    DEV: LogoSprite

--- src/components/misc/LogoSprite.js
+++ src/components/misc/LogoSprite.js
@@ -6,16 +6,77 @@ import '../../stylesheets/misc/logoSprite.css'
 
 class LogoSprite extends React.Component {
   state = {
-    logoSprite: logoSprite,
+    img: null,
     frames: 16,
-    leftFrame: 5000
+    frameIndex: 0,
+    ctx: null
+  }
+
+  componentDidMount() {
+    this.setCanvas()
+    this.setImage()
+  }
+
+  componentDidUpdate() {
+
+    
+    if (this.state.frameIndex < this.state.frames) {
+      this.drawLogo()
+
+      const { frameIndex } = this.state
+      const newFrameIndex = frameIndex + 1
+
+      console.log(frameIndex)
+      setInterval(
+        () => {
+          this.setState({
+            frameIndex: newFrameIndex
+          })
+        },
+        100
+      )
+    }
+  }
+
+  setCanvas = () => {
+    const canvas = document.getElementById("logoCanvas")
+    canvas.height = 236
+    canvas.width = 1138
+    const ctx = canvas.getContext("2d")
+
+    this.setState({
+      ctx: ctx
+    })
+  }
+
+  setImage = () => {
+    var img = new Image
+    img.src = logoSprite
+
+    this.setState({
+      img: img
+    })
+  }
+
+  drawLogo() {
+    const { img, frames, frameIndex, ctx } = this.state
+
+    ctx.drawImage(
+      img,
+      frameIndex * img.width / frames,
+      0,
+      img.width / frames,
+      img.height,
+      0,
+      0,
+      1138,
+      236
+    )
   }
 
   render() {
-    const { logoSprite, leftFrame } = this.state
-
     return (
-      <img src={logoSprite} alt="taskbleLogoSprite" left={leftFrame} id="sprite"/>
+      <canvas id='logoCanvas'/>
     )
   }
 }
