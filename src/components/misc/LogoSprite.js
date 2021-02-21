10ebe8adae0a6d77dafd6fa3349bd970864ae741
cromerohernandez <c.romero.hsv@gmail.com>
2021-02-22 00:12:10 +0100

    DEV: LogoSprite(=>Hooks)

--- src/components/misc/LogoSprite.js
+++ src/components/misc/LogoSprite.js
@@ -1,10 +1,72 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import logoSprite from '../../assets/images/taskbleLogoSprite.png'
 
 import '../../stylesheets/misc/logoSprite.css'
 
-class LogoSprite extends React.Component {
+const LogoSprite = () => {
+  const [img, setImg] = useState(null)
+  const [frames, setFrames] = useState(16)
+  const [frameIndex, setFrameIndex] = useState(0)
+  const [ctx, setCtx] = useState(null)
+
+  useEffect(() => {
+    setCanvas()
+    setImage()
+    console.log(ctx)
+  }) 
+
+  useEffect(() => {
+    if (frameIndex < frames) {
+      const newFrameIndex = frameIndex + 1
+
+      clearLogo()
+      drawLogo(ctx)
+      setTimeout(() => {setFrameIndex(newFrameIndex)}, 40)
+    }
+  }, [ctx])
+
+  const setCanvas = () => {
+    const canvas = document.getElementById("logoCanvas")
+    canvas.width = window.innerWidth/4.2
+    canvas.height = canvas.width/4.82
+    const ctx = canvas.getContext("2d")
+
+    setCtx(ctx)
+  }
+
+  const setImage = () => {
+    var img = new Image()
+    img.src = logoSprite
+
+    setImg(img)
+  }
+
+  const drawLogo = (ctx) => {
+     ctx.drawImage(
+      img,
+      frameIndex * img.width / frames,
+      0,
+      img.width / frames,
+      img.height,
+      0,
+      0,
+      ctx.canvas.width,
+      ctx.canvas.height
+    )
+  }
+
+  const clearLogo = () => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+  }
+
+  return <canvas id='logoCanvas'/>
+}
+
+export default LogoSprite
+
+
+/*class LogoSprite extends React.Component {
   state = {
     img: null,
     frames: 16,
@@ -82,6 +144,4 @@ class LogoSprite extends React.Component {
       <canvas id='logoCanvas'/>
     )
   }
-}
-
-export default LogoSprite
+}*/
