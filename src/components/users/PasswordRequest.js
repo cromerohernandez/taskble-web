8de91c60af72ea5b269f97c852829afb2d06ad03
cromerohernandez <c.romero.hsv@gmail.com>
2021-03-26 18:59:50 +0100

    DEV: PasswordRequest

--- src/components/users/PasswordRequest.js
+++ src/components/users/PasswordRequest.js
@@ -1,13 +1,71 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
+
+import TaskbleService from '../../services/TaskbleService'
+
+import useInput from '../../hooks/useInput'
+
+import Input from '../UI/Input'
+
+// eslint-disable-next-line
+const EMAIL_PATTERN = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+
+const validators = {
+  email: val => val.match(EMAIL_PATTERN)
+}
+
+const errorMessages = {
+  email: 'invalid email format',
+}
 
 const PasswordRequest = () => {
+  const history = useHistory()
+
+  const {
+    value: email,
+    touch: emailTouch,
+    error: emailError,
+    handleInput: emailHandleInput 
+  } = useInput('', validators.email, errorMessages.email)
+
+  const anyError = () => {
+    const errors = [emailError.active]
+    return errors.some(x => x === true)
+  }
+
+  const handleRequestNewPassword = (event) => {
+    event.preventDefault()
+    const data = {email}
+
+    TaskbleService.requestNewPassword(data)
+      .then(() => {
+        //////////////////////////////////////////////// => ADD ALERT !!!!!
+        history.push('/')
+      })
+      .catch(() => {
+        //////////////////////////////////////////////// => ADD ALERT !!!!!
+      })
+  }
 
   return (
     <div id='passwordRequest'>
 
       <h3>Enter your email, and we'll send you a link to get back into your account.</h3>
 
-      <h3>Send login link</h3>
+      <form onSubmit={handleRequestNewPassword} /*id='form-container'*/>
+
+        <Input type='text' name='email' {...emailHandleInput} />
+          {emailTouch && emailError.active && (
+            <div>
+              { emailError.message }
+            </div>
+          )}
+
+        <button disabled={anyError()} type="submit" /*id='form-submitButton'*/>
+          Send login link
+        </button>
+
+      </form>
 
     </div>
   )
