b2039bd6ccb99003e08fe064bdbea8f92cca294b
cromerohernandez <c.romero.hsv@gmail.com>
2021-05-05 23:56:54 +0200

    DEV: Calendar & tasksHelper

--- src/helpers/tasksHelper.js
+++ src/helpers/tasksHelper.js
@@ -1,3 +1,10 @@
+export const getFirstDayOfTheWeek = (dateInMilliseconds) => {
+  const weekday = (new Date(dateInMilliseconds)).getDay()
+  const oneDayInMiliseconds = 24 * 60 * 60 * 1000
+
+  return dateInMilliseconds - (weekday * oneDayInMiliseconds)
+}
+
 export const sortByFinalPriority = (tasks) => {
   return tasks.sort((a, b) => b.finalPriority - a.finalPriority)
 }
