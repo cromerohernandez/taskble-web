55243288aee172436325de08efeef79c82f61ed4
cromerohernandez <c.romero.hsv@gmail.com>
2021-05-24 13:56:44 +0200

    DEV: Task & TaskForm

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-const Input = ({ type, name, placeholder, value, onChange, onBlur }) => {
+const Input = ({ type, name, placeholder, disabled, value, onChange, onBlur }) => {
   return (
     <input
      /*id='form-error'*/
     type={type}
     name={name}
     placeholder={placeholder ? placeholder : name}
+    disabled={disabled ? disabled : false}
     defaultValue={value}
     onChange={onChange}
     onBlur={onBlur}
