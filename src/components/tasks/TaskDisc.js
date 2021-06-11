6dafee91d0b55b5390acb3226a8901df3f8a08c4
cromerohernandez <c.romero.hsv@gmail.com>
2021-06-11 12:03:03 +0200

    DEV: TaskDisc (done)

--- src/components/tasks/TaskDisc.js
+++ src/components/tasks/TaskDisc.js
@@ -1,20 +1,51 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+
+import TranslateContext from '../../contexts/TranslateContext'
+import TaskbleService from '../../services/TaskbleService'
 
 import TaskModal from './TaskModal'
 
+import Button from 'react-bootstrap/Button'
+
 const TaskDisc = ({ task }) => {
+  const { texts } = useContext(TranslateContext)
+
+  const [overDisc, setOverDisc] = useState(false)
   const [show, setShow] = useState(false)
+
+  const handleOverDisc = () => setOverDisc(true)
+
+  const handleOutDisc = () => setOverDisc(false)
+
+  /*const handleDone = () => {
+    TaskbleService.doneTask(task.id)
+      .then(() => {
+        //getTask()
+        //////////////////////////////////////////////// => ADD ALERT !!!!!
+      })
+      .catch(() => {
+        //////////////////////////////////////////////// => ADD ALERT !!!!!
+      })
+  }*/
 
   const handleShow = () => setShow(true)
 
   return (
-    <div>
-      {task && (
-        <div onClick={handleShow}>
-          <h6>{task.title}</h6>
-          <h6>{task.finalPriority}</h6>
-        </div>
-      )}
+    <div onMouseOver={handleOverDisc} onMouseOut={handleOutDisc}>
+      <div>
+        {task && (
+          <div onClick={handleShow}>
+            <h6>{task.title}</h6>
+            <h6>{task.finalPriority}</h6>
+          </div>
+        )}
+
+        {overDisc && (
+          <Button variant={task.done ? 'success' : 'warning'} /*onClick={handleDone}*/>
+            {task.done ? texts.buttons.doneTask : texts.buttons.pendingTask}
+          </Button>
+        )}
+      </div>
       
       {show && (
         <TaskModal taskId={task.id} typeModal={'view'} show={show} setShow={setShow} />
